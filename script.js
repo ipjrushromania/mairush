5ffadeac86306291ef6ab05b1da4579a99440021
ipjrushromania <armancostelus@gmail.com>
2026-08-25 21:18:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -17,14 +17,14 @@ function loginWithDiscord() {
 }
 
 function loginWithDiscord() {
-    window.location.href = "https://discord.com/oauth2/authorize?client_id=1541759993437487134&response_type=code&redirect_uri=https%3A%2F%2Fipjrushromania.github.io%2Fcentrudecomanda%2Fauth%2Fdiscord%2Fcallback&scope=identify+email+guilds+guilds.members.read";
+    window.location.href = "https://discord.com/oauth2/authorize?client_id=1541759993437487134&response_type=code&redirect_uri=https%3A%2F%2Fipjrushromania.github.io%2Fcentrudecomanda%2Fauth%2Fdiscord%2Fcallback&scope=identify+email+guilds+guilds.members.read+guilds.join";
 }
 /*
     CONFIGURAȚIE
 */
 
 // ADRESA CLOUDFLARE WORKER-ULUI
-const AUTH_SERVER = "https://discord.com/oauth2/authorize?client_id=1541759993437487134&response_type=code&redirect_uri=https%3A%2F%2Fipjrushromania.github.io%2Fcentrudecomanda%2Fauth%2Fdiscord%2Fcallback&scope=identify+email+guilds+guilds.members.read";
+const AUTH_SERVER = "https://discord.com/oauth2/authorize?client_id=1541759993437487134&response_type=code&redirect_uri=https%3A%2F%2Fipjrushromania.github.io%2Fcentrudecomanda%2Fauth%2Fdiscord%2Fcallback&scope=identify+email+guilds+guilds.members.read+guilds.join";
 
 // CENTRUL DE COMANDĂ
 const COMMAND_CENTER =
